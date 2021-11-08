99a558a06c1b08d6c5de8f9132f885d738a8e1a9
pm.poletaev <ppolet@mail.ru>
2021-11-08 16:39:07 +0300

    Добавил 2 игру с использованием цифр без повторов

--- src/guessthenumber/Game.java
+++ src/guessthenumber/Game.java
@@ -1,8 +1,11 @@
 package guessthenumber;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Game {
     private String number; //загаданное число
@@ -10,12 +13,26 @@ public class Game {
     private UserMove move; 
     private List<UserMove> historyMoves;
     
-    //загадываем новое число
-    private void newNumber(){
+    //загадываем новое число с повторяющимися цифрами
+    private void newNumberWithRepeating(){
         number = "";
         for(int i = 0; i < 4; i++){
             number = number + (int)(Math.random()*10);
         }
+    }
+
+    //загадываем новое число без повторяющихся цифр
+    private void newNumberNoRepeating(){
+        List<Integer> n = new ArrayList<>(Arrays.asList(0,1,2,3,4,5,6,7,8,9));
+        number = "";
+        for(int i = 0; i < 4; i++){
+            int index = (int)(Math.random()*n.size());
+            number = number + n.get(index);
+            n.remove(index);
+            //Set<Integer> aaa = new HashSet<>(Arrays.asList(0,1,2,3,4,5,6,7,8,9));
+            //aaa.remove(i);
+        }
+        //System.out.println("NUMBER: " + number);
     }
 
     //проверяем число пользователя
@@ -58,9 +75,39 @@ public class Game {
         return result;
     }
     
+    //ввод числа - выбора варианта игры 1 или 2, если другой символ, то выход из игры
+    private int gameChoice(){
+        Scanner sc = new Scanner(System.in);
+        int n = 0;
+        if(sc.hasNextInt()){
+            n = sc.nextInt();
+        }
+        return n;
+    }
+    
     public void startGame(){
+        System.out.println(" -= Игра \"Угадай число\" =-");
+        System.out.print("1 - угадать число от 0000 до 9999 с повторениями цифр\n"
+                + "2 - угадать число без повторения цифр\n"
+                + "Выберите игру: ");
+        
+        switch (gameChoice()){
+            case 1: 
+                System.out.println("Игра с повторяющимися цифрами в загаданном числе");
+                newNumberWithRepeating();
+                break;
+            case 2:
+                System.out.println("Игра без повторяющихся цифр в загаданном числе");
+                newNumberNoRepeating();
+                break;
+            default:
+                System.out.println("Вы не ввели 1 или 2. Выходим...");
+                return;
+        }
+
+        System.out.println("");
         System.out.println("Угадай число. Введите 4-х значное число. Или слово quit для выхода");
-        newNumber();
+
         historyMoves = new ArrayList<>();
         
         do{
